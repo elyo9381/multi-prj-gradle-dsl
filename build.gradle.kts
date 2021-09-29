c8c689cd54284c73857a473a0bcc4fd5a910d08d
elyo9381 <won4413@daum.net>
2021-09-29 11:21:02 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,54 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "2.3.5.RELEASE" apply false
+    id("io.spring.dependency-management") version "1.0.10.RELEASE" apply false
+
+    kotlin("plugin.spring") version "1.3.72" apply false
+    kotlin("plugin.jpa") version "1.3.72" apply false
+    kotlin("jvm") version "1.3.72"
+    kotlin("kapt") version "1.3.72" apply false
+}
+
+
+allprojects {
+    group = "com.movv"
+    version = "0.0.1-SNAPSHOT"
+//    var tag = SimpleDateFormat("MMddHHmm").format(Date())
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "11"
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
+}
+
+
+subprojects {
+    repositories {
+        mavenCentral()
+    }
+    apply {
+        plugin("kotlin")
+        plugin("kotlin-spring")
+        plugin("kotlin-kapt")
+        plugin("kotlin-jpa")
+        plugin("idea")
+        plugin("eclipse")
+        plugin("org.springframework.boot")
+        plugin("io.spring.dependency-management")
+        plugin("kotlin-allopen")
+    }
+
+}
+
+
+rootProject.subprojects.forEach { subproject ->
+    "api-".filter { !subproject.name.startsWith(it) }.map { subproject.tasks["bootJar"].enabled = false }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,48 @@
+rootProject.name = "multi-module-prj"
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+    }
+}
+
+
+val GRADLE_TEXT = """
+
+    dependencies {
+
+    }
+
+    """
+listOf("core", "api").forEach { moduleName ->
+
+    val compDir = File(rootDir, moduleName)
+    if(!compDir.exists()){
+        compDir.mkdirs()
+    }
+
+    compDir.listFiles().forEach { subDir ->
+
+        val buildFile = File(subDir.absolutePath, "build.gradle.kts")
+        if (!buildFile.exists()) {
+            buildFile.writeText(GRADLE_TEXT)
+        }
+
+
+        listOf( "src/main/kotlin/com/movv/",
+            "src/main/resources",
+            "src/test/kotlin/com/movv/",
+            "src/test/resources")
+            .forEach {srcDir->
+                val srcFolder = File(subDir.absolutePath, srcDir)
+                if(!srcFolder.exists()){
+                    srcFolder.mkdirs()
+                }
+            }
+
+        val projectName = ":${moduleName}-${subDir.name}"
+        include(projectName)
+        project(projectName).projectDir = subDir
+
+    }
+
+}
